1b0500c46257bdec01f46b583ff2fc858ad3db8e
Drawercho <liasun97@gmail.com>
2026-04-24 17:20:25 +0900

    fix: simplify open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,28 +1,5 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => {
-        const { KVIncrementalCache } = await import(
-          "@opennextjs/cloudflare/kv-cache"
-        );
-        return new KVIncrementalCache();
-      },
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
+const config: OpenNextConfig = {};
 
 export default config;
